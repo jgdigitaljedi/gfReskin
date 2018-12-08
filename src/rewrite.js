33532b33d65d8987719ad5bc4a54f93737711fe3
Joey Gauthier <digitaljedi@outlook.com>
2018-12-08 07:32:00 -0600

    made it through edit post and user cp

--- src/rewrite.js
+++ src/rewrite.js
@@ -177,4 +177,319 @@ imgArr.forEach(item => {
       value: "1px"
     });
   }
+
+  // down arrow beside menu item
+  if (
+    item.src === "https://www.guitarforums.com/guitarforum/misc/menu_open.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-sort-down"], "#fbfbf2");
+  }
+
+  // smile wink status icon
+  if (item.src === "https://www.guitarforums.com/images/icons/icon12.gif") {
+    changeIcon(item, ["fas", "fa-smile-wink"], "#87BCDE");
+  }
+
+  // poll status icon
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/poll_posticon.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-poll-h"], "#083D77");
+  }
+
+  // post edit remove formatting
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/editor/removeformat.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-ban"], "#083D77", {
+      prop: "padding",
+      value: "4px"
+    });
+  }
+
+  // post edit bold
+  if (item.src === "https://www.guitarforums.com/guitarforum/editor/bold.gif") {
+    changeIcon(item, ["fas", "fa-bold"], "#083D77", {
+      prop: "padding",
+      value: "4px"
+    });
+  }
+
+  // post edit italic
+  if (
+    item.src === "https://www.guitarforums.com/guitarforum/editor/italic.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-italic"], "#083D77", {
+      prop: "padding",
+      value: "4px"
+    });
+  }
+
+  // post edit underline
+  if (
+    item.src === "https://www.guitarforums.com/guitarforum/editor/underline.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-underline"], "#083D77", {
+      prop: "padding",
+      value: "4px"
+    });
+  }
+
+  // post edit image
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/editor/insertimage.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-image"], "#083D77", {
+      prop: "padding",
+      value: "4px"
+    });
+  }
+
+  // post edit link
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/editor/createlink.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-link"], "#083D77", {
+      prop: "padding",
+      value: "4px"
+    });
+  }
+
+  // post edit quote
+  if (
+    item.src === "https://www.guitarforums.com/guitarforum/editor/quote.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-quote-right"], "#083D77", {
+      prop: "padding",
+      value: "4px"
+    });
+  }
+
+  // post edit color
+  if (
+    item.src === "https://www.guitarforums.com/guitarforum/editor/color.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-palette"], "#083D77", {
+      prop: "padding",
+      value: "4px"
+    });
+  }
+
+  // post edit color under part
+  if (item.src === "https://www.guitarforums.com/clear.gif") {
+    const parent = item.parentNode;
+    parent.removeChild(item);
+  }
+
+  // post edit down arrow
+  if (
+    item.src === "https://www.guitarforums.com/guitarforum/editor/menupop.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-sort-down"], "#083D77", {
+      prop: "padding",
+      value: "4px"
+    });
+  }
+
+  // post resize up arrow
+  if (
+    item.src === "https://www.guitarforums.com/guitarforum/editor/resize_0.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-angle-double-up"], "#083D77", {
+      prop: "padding",
+      value: "4px"
+    });
+  }
+
+  // post resize down arrow
+  if (
+    item.src === "https://www.guitarforums.com/guitarforum/editor/resize_1.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-angle-double-down"], "#083D77", {
+      prop: "padding",
+      value: "4px"
+    });
+  }
+
+  // currently active users
+  if (
+    item.src === "https://www.guitarforums.com/guitarforum/misc/whos_online.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-users", "fa-2x"], "#A62639", {
+      prop: "padding",
+      value: "4px"
+    });
+  }
+
+  // stats
+  if (item.src === "https://www.guitarforums.com/guitarforum/misc/stats.gif") {
+    changeIcon(item, ["fas", "fa-chart-line", "fa-2x"], "#083D77", {
+      prop: "padding",
+      value: "4px"
+    });
+  }
+
+  // user cp edit details
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/user_edit.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-user-edit"], "#A62639");
+  }
+
+  // user cp edit privacy
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/key.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-user-secret"], "#A62639");
+  }
+
+  // user cp edit profile picture
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/vcard_edit.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-camera-retro"], "#A62639");
+  }
+
+  // user cp edit avatar
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/picture_edit.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-user-circle"], "#A62639");
+  }
+
+  // user cp edit signature
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/page_red.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-signature"], "#A62639");
+  }
+
+  // user cp email and password
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/lock.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-key"], "#A62639");
+  }
+
+  // user cp edit options
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/cog.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-cog"], "#A62639");
+  }
+
+  // user cp edit ignore list
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/user_delete.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-shield-alt"], "#A62639");
+  }
+
+  // user cp edit reminders
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/time.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-bell"], "#A62639");
+  }
+
+  // user cp attachments
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/attach.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-paperclip"], "#A62639");
+  }
+
+  // user cp contacts and friends
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/user_add.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-user-friends"], "#083D77");
+  }
+
+  // user cp inbox
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/email_open_image.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-inbox"], "#083D77");
+  }
+
+  // user cp sent items
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/email_go.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-share-square"], "#083D77");
+  }
+
+  // user cp list messages
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/email_open.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-mail-bulk"], "#083D77");
+  }
+
+  // user cp send new message
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/email_add.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-envelope"], "#083D77");
+  }
+
+  // user cp track messages
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/email_link.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-file-contract"], "#083D77");
+  }
+
+  // user cp edit folders
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/folder_edit.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-folder-plus"], "#083D77");
+  }
+
+  // user cp edit subscriptions
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/misc/usercp_icons/newspaper.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-newspaper"], "#083D77");
+  }
+
+  // collapse thread
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/buttons/collapse_thead.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-angle-double-up"], "#fbfbf2");
+  }
+
+  // collapse thread alt
+  if (
+    item.src ===
+    "https://www.guitarforums.com/guitarforum/buttons/collapse_tcat.gif"
+  ) {
+    changeIcon(item, ["fas", "fa-angle-double-up"], "#fbfbf2");
+  }
 });
